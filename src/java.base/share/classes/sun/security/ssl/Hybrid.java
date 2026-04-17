1dd8cd3552f6b17b11c27441c483f7d9e8e47f1a
JinhangZhang <Jinhang.Zhang@ibm.com>
2026-04-17 12:49:05 -0400

    update the name

--- src/java.base/share/classes/sun/security/ssl/Hybrid.java
+++ src/java.base/share/classes/sun/security/ssl/Hybrid.java
@@ -109,7 +109,7 @@ public class Hybrid {
         if (name.startsWith("secp") || name.equals("X25519")) {
             return KEM.getInstance("DH", HybridProvider.PROVIDER);
         } else {
-            return KEM.getInstance("ML-KEM");
+            return KEM.getInstance(name);
         }
     }
 
